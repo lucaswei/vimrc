37dde6c96eb99e063d07e9ecd553571b120e917d
Lucas <lucas.weisr@gmail.com>
2011-12-29 11:58:47 +0800

    install

---
+++ auto-install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+VIMHOME=~/.vim
+
+die() {
+    warn "$1"
+    exit 1
+}
+
+[ -e "$VIMHOME/vimrc" ] && die "$VIMHOME/vimrc already exists."
+[ -e "~/.vim" ] && die "~/.vim already exists."
+[ -e "~/.vimrc" ] && die "~/.vimrc already exists."
+
+git clone git://github.com/vgod/vimrc.git "$VIMHOME"
+cd "$VIMHOME"
+git submodule update --init
+
+cd ..
+ln -s .vim/vimrc .vimrc
+
+echo "vimrc is installed."
+
+#reference by 
+#vgod's auto-install.sh
